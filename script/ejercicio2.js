8bc0c961e89376706cdf90abc00d8fd959da2b12
Albertojose241434 <albertojose241434@gmail.com>
2023-04-26 15:04:50 -0500

    porfin

---
+++ script/ejercicio2.js
@@ -0,0 +1,17 @@
+let nombre1, nombre2;
+let deducciones1, bonificaciones1, bruto1, neto1;
+let deducciones2, bonificaciones2, bruto2, neto2;
+
+
+//usuario1
+nombre1 = prompt('ingrese el nombre del usuario 1')
+deducciones1 = Number(prompt('ingrese las deducciones del usuario 1'))
+bonificaciones1 = Number(prompt('ingrese las bonificaciones del usuario 1'))
+bruto1 = Number (prompt('ingrese el salario del usuario 1'))
+
+//usuario2
+nombre2 = prompt('ingrese el nombre del usuario 2')
+deducciones2 = Number(prompt('ingrese las deducciones del usuario 2'))
+bonificaciones2 = Number(prompt('ingrese las bonificaciones del usuario 2'))
+bruto2 = Number (prompt('ingrese el salario del usuario 2'))
+
